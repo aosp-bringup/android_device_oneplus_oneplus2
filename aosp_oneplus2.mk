00f3903c5c6aa267b562da6dbad3cf72c24de2db
k2wl <kar2wlan@gmail.com>
2016-09-30 13:22:15 +0530

    aosp bringup part 1
    Signed-off-by: k2wl <kar2wlan@gmail.com>

---
+++ aosp_oneplus2.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2013 The Android Open Source Project
+# Copyright 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+$(call inherit-product, device/oneplus/oneplus2/device.mk)
+
+# Overlay and packages usually set by vendor/cm
+DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus2/overlay2
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false \
+    ro.build.selinux=1
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+# Enable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+endif
+
+# Set parameters
+PRODUCT_DEVICE := oneplus2
+PRODUCT_NAME := aosp_oneplus2
+PRODUCT_BRAND := oneplus
+PRODUCT_MANUFACTURER := oneplus
+PRODUCT_MODEL := ONEPLUS2
